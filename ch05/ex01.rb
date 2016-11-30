72189b994c1e89e57b1c84fda42b000db02e4fe0
Ben Kielty <bwk103@gmail.com>
2016-11-30 11:01:07 +0000

    Chapter 5

---
+++ ch05/ex01.rb
@@ -0,0 +1,8 @@
+puts 'Hello there, what is your first name?'
+first_name = gets.chomp
+puts 'Thank you. Now, can I please have your middle name?'
+middle_name = gets.chomp
+puts 'Great.  Finally, can I please have your last name?'
+last_name = gets.chomp
+
+puts 'So, your full name is ' + first_name + ' ' + middle_name + ' ' + last_name +'? Great to meet you, ' + first_name + '.'
---
+++ ch05/ex02.rb
@@ -0,0 +1,4 @@
+puts "Hi there, what's your favourite number?"
+fav_num = gets.chomp.to_i
+puts "Oh, your favourite number is #{fav_num}?"
+puts "That's all well and good, but I think you'll find that #{fav_num + 1} is a much better number!"
